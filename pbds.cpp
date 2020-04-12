eeb8b2cded7d3781df7e84b05c9d4e052ed9a0df
bhupixb <bhupendrayadavxb@gmail.com>
2020-04-12 17:51:05 +0530

    Policy Based Data Structure in c++

---
+++ pbds.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+using namespace std;
+
+#define int long long
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define pi pair<int,int> 
+#define sz(s) ((int)s.size())
+#define rep(i,a,b) for (int i = a; i <= (int)b; ++i)
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> Set;
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> SetPair;
+ 
+#define less_than_i(i) st.order_of_key(i)
+#define ith_index(i) *st.find_by_order(i)
+
+const unsigned mod = 1e9 + 7;
+const unsigned N = 5e5 + 4;
+
+signed main() {
+  fast;
+	
+  
+
+
+
+
+  
+  assert(numeric_limits<int>::max() >= 1e18);
+  return 0;
+}
