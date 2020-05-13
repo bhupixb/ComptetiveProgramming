c25ddca42bddae983d8d9f5b3b8b11dc6e39cfca
bhupixb <bhupendrayadavxb@gmail.com>
2020-05-13 14:28:44 +0530

    Catching silly mistakes in c++ using f-sanitize etc.

---
+++ debug.cpp
@@ -0,0 +1,6 @@
+# run it like -> bash thisFileName.sh file_name_of_cpp_to_be_excuted
+if [ -z "$2" ]
+	then
+		g++ -Wall -Wextra -std=c++11 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector $1.cpp && ./a.out < input.txt	
+else
+	g++ -Wall -Wextra -std=c++11 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector $1.cpp && ./a.out 
